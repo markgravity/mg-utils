9bac9447ad0feaa65f42015c6d8b4f7cb5e565fb
Mark G <markgravity.in@gmail.com>
2016-09-18 11:08:03 +0700

    add sourcce

---
+++ mg-utils/mg-utils.h
@@ -0,0 +1,19 @@
+//
+//  mg-utils.h
+//  mg-utils
+//
+//  Created by Mark G on 9/18/16.
+//  Copyright © 2016 Mark G. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for mg-utils.
+FOUNDATION_EXPORT double mg_utilsVersionNumber;
+
+//! Project version string for mg-utils.
+FOUNDATION_EXPORT const unsigned char mg_utilsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <mg_utils/PublicHeader.h>
+
+
